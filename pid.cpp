682a1af2c1fd07884139205e2e335bbe3e4bbb90
Paul-Mierscheidt <paumi7239@gmail.com>
2026-06-30 15:49:41 +0200

    Add PID control logic to pid function
    Implement PID control calculations including proportional, integral, and differential parts.

--- pid.cpp
+++ pid.cpp
@@ -2,9 +2,23 @@
 
 float pid(pid_settings_t &s, float soll, float in) {
   //code needs to be added here
-  float pid = 0.0;
+ float pid = 0.0;
+  float current_error = soll - in;
+  
+  //Proportional part
+  float u_P = s.p * current_error;
+  
+  //Integral part
   
 
+  //Differential part
+  float u_D = s.d * ((current_error-last_error)/T)
+
+  //PID 
+  float pid = u_P + u_I + u_D;
+
+  last_error = current_error;
+  reset_integrator(&s);
   //pid=s.p+s.i+s.d; so richtig?
   return pid;
 }
