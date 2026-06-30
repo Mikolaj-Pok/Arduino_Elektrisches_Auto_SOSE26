1a626fb8926100431eccf35b3c34cafc8fa7f322
benediktvosspv-max <benedikt.voss.pv@gmail.com>
2026-06-30 13:43:20 +0200

    Update servo.cpp

--- servo.cpp
+++ servo.cpp
@@ -8,19 +8,20 @@ Servo servo_object;
 void servo_setup(servo_settings_t &s) {
   //code needs to be added here
   servo_object.attach(s.pin);
-  
+  sgvsgrsvfehtehtbehteht
   pinMode(s.pin, OUTPUT);
 }
 
 
 void servo_set_position(servo_settings_t &s, int pos) {
   //code needs to be added here
-servo_object.write(pos); 
+  int micro_pos = s.full_left + (s.full_right - s.full_left) * pos/180;
+  servo_object.writeMicroseconds(micro_pos); 
 
 }
 
 void servo_test(servo_settings_t &s) {
   //code needs to be added here
-
+  fahr nach links, setz wert, fahr nachts rechts, setzt wert, setz center in der mitte, fahr zu center
   
 }
