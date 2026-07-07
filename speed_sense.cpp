857fba36b4622fee81ca79f9895ee66803f379b6
Paul-Mierscheidt <paumi7239@gmail.com>
2026-07-07 11:34:13 +0200

    Update speed_sense.cpp

--- speed_sense.cpp
+++ speed_sense.cpp
@@ -6,5 +6,5 @@ void setup_speed_sense(speed_sense_settings_t &s) {
 
   pinMode(s.pin, INPUT);
   
-  attachInterrupt(digitalPinToInterrupt(s.pin), speed_interrupt, RISING);
+  attachInterrupt(digitalPinToInterrupt(s.pin), speed_interrupt(), RISING);
 }
