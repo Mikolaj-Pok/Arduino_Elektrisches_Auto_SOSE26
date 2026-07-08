f871512ab80514b60a34e9f9568f0cca4da758db
Mikolaj Pokrywinski <mikipokpok@gmail.com>
2026-07-08 13:53:38 +0200

    Probleme mit der Geschwindigkeitsregelung

--- Institutsprojekt_Auto_4Sem.ino
+++ Institutsprojekt_Auto_4Sem.ino
@@ -39,16 +39,16 @@ sensor_settings_t right_sensor = {
 //the entries should not rearranged (compiler error)
 pid_settings_t direction_control = {
   .p = 2.5,
-  .i = 0,
-  .d = 0,
-  .anti_windup = 50.0,
+  .i = 0.0,
+  .d = 1.0,
+  .anti_windup = 50.0, //egal, integral unbenutzt
   .integral = 0.0,
   .last_error = 0.0
 };
 
 //the entries should not rearranged (compiler error)
 pid_settings_t speed_control = {
-  .p = 0.5,
+  .p = 2.5,
   .i = 0.0,
   .d = 0.0,
   .anti_windup = 20.0,
@@ -80,8 +80,6 @@ void setup() {
   uart_attach_servo(servo);
   uart_attach_settings(settings);
   uart_attach_motor(motor);
-  unsigned int v_last = motor.max_speed;
-  motor_set_speed(motor, v_last); //starts the motor once
 }
 
 //interrupt for measuring speed from speed sensor
@@ -90,7 +88,7 @@ void speed_interrupt() {
   // attachInterrupt(digitalPinToInterrupt(pin), ISR, modus) interrupts the code
   // to measure the speed, continues the code afterwards
 
-  unsigned long current_time=micros(); //micros(): return the passed time in microseconds since the start of the code
+  unsigned long current_time=millis(); //millis(): return the passed time in milliseconds since the start of the code
   speed_sense.time_diff=current_time-speed_sense.last_time;
   speed_sense.last_time=current_time;
 
@@ -99,6 +97,10 @@ void speed_interrupt() {
 float aktueller_fehler= 0.0;
 float direction= 0.0;
 float lenkwinkel= 90.0;
+unsigned int v_soll=0;
+unsigned int geschwindigkeit=0.0;
+unsigned int v_last = motor.max_speed;
+
 
 void loop() {
 
@@ -109,6 +111,7 @@ void loop() {
   aktueller_fehler = (float)left_sensor.value - (float)right_sensor.value; //maximum value ~  8 ; float cast solves underflow with unsigned floats
   if(abs(aktueller_fehler) < 2.0){
     aktueller_fehler = 0.0;  //Threshold for minimal errors, shall prevent jittering
+    reset_integrator(speed_control);
       }
   
   direction = pid(direction_control, 0.0, aktueller_fehler);
@@ -125,7 +128,7 @@ void loop() {
   float v_kmh=0.0;
   //calculate Speed in km/h
   if (speed_sense.time_diff > 0.0) {
-  float period_s = speed_sense.time_diff / 1000000.0;   // micros → seconds
+  float period_s = speed_sense.time_diff / 1000.0;   // millis → seconds
   float freq = 1.0 / period_s;                          // Hz
   const float radumfang = 0.1885;                       // Meter per rotation
 
@@ -135,9 +138,9 @@ void loop() {
 */
 
   //the larger the current error, the slower the car should go
-  float v_soll = motor.max_speed - 0.5 * abs(aktueller_fehler);
-  v_soll=constrain(v_soll, motor.min_speed, motor.max_speed);
-  float geschwindigkeit = pid(speed_control, v_soll, v_last);
+  v_soll = motor.max_speed - 1.0 * abs(aktueller_fehler);
+  v_soll = constrain(v_soll, motor.min_speed, motor.max_speed);
+  geschwindigkeit = pid(speed_control, v_soll, v_last);
   
   motor_set_speed(motor, geschwindigkeit);
   v_last = geschwindigkeit; 
@@ -148,17 +151,20 @@ void loop() {
     Serial.print("Left_avg:");
     Serial.print(left_sensor.value);
     sensor_read(right_sensor);
-    Serial.print(",Right_avg:");
+    Serial.print(" ,Right_avg:");
     Serial.print(right_sensor.value);
-    Serial.print(",Left:");
+    Serial.print(" ,Left:");
     Serial.print(analogRead(left_sensor.pin));
-    Serial.print(",Right:");
+    Serial.print(" ,Right:");
     Serial.print(analogRead(right_sensor.pin));
-    Serial.print('\n');
-   /* Serial.print(",Controller:");
+    Serial.print(" ,Controller:");
     Serial.print(direction);
+    Serial.print(" ,Fehler");
+    Serial.print(aktueller_fehler);
+    Serial.print(" ,Geschwindigkeit");
+    Serial.print(geschwindigkeit);
     Serial.println("");
-    Serial.print("Speed:");
+    /* Serial.print("Speed:");
     Serial.print(digitalRead(speed_sense.pin));
     Serial.print('\n'); */
     delay(50);
